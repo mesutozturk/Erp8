713d57ac9c8c7ef89ccc37fd7f7a5f36de5cb130
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2023-02-23 11:48:31 +0300

    adam asmaca v1 ok

--- AdamAsmaca/Program.cs
+++ AdamAsmaca/Program.cs
@@ -1,10 +1,24 @@
 ﻿using AdamAsmaca.Helpers;
 
-GameHelper game = new GameHelper();
-Random rnd = new Random();
+ConsoleKey key;
 do
 {
-    Console.WriteLine(game.EkraniYaz());
-    game.TahminYap(Console.ReadLine());
-} while (game.OyunAktifMi());
+    GameHelper game = new GameHelper();
+    Random rnd = new Random();
+    do
+    {
+        Console.WriteLine(game.EkraniYaz());
+        game.TahminYap(Console.ReadLine());
+    } while (game.OyunAktifMi());
 
+    if (game.HakKontrol() > 0)
+    {
+        Console.WriteLine(game.SeciliSoru);
+        Console.WriteLine("Tebrikler Bildiniz");
+        Console.WriteLine($"Puanınız: {game.PuanKontrol()}");
+    }
+
+    Console.WriteLine("Tekrar oynamak için E tuşuna basınız");
+
+    key = Console.ReadKey().Key;
+} while (key == ConsoleKey.E);
