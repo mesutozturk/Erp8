b1deb45724c2e544883d58d795ad529820157aef
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2023-02-24 09:16:55 +0300

    .

---
+++ OopGiris/Kisi.cs
@@ -0,0 +1,10 @@
+﻿namespace OopGiris;
+public class Kisi
+{
+    //ad, soyad, dogumTarihi, tckn, telefon, email, yas
+    //Ad ve Soyad da özel karakter ve sayı olmamalı. Okunurken ilk harf büyük sonraki harfler küçük şekilde gösterilmeli
+    //TCKN 11 haneli olmalı ve sadece rakamlardan oluşmalı
+    //Telefon 10 haneli olmalı ve sadece rakamlardan oluşmalı
+    //Email adresi @ işaretinden sonra en az 2 karakter olmalı ve email kurallarına uygun olmalı asd@asd
+    //yas özelliği sadece okunur olmalı
+}
