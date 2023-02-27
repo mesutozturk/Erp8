2a1cf3bc678672cd4e1de9b6fce94d6ed344dccc
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2023-02-27 15:01:05 +0300

    TODO

--- WfaGiris/FormKisiler.cs
+++ WfaGiris/FormKisiler.cs
@@ -30,13 +30,30 @@ namespace WfaGiris
                     Telefon = txtTelefon.Text
                 };
 
-                lstKisiler.DisplayMember = "Ad";
+                //lstKisiler.DisplayMember = "Ad";
                 lstKisiler.Items.Add(yeniKisi);
+                FormuTemizle();
             }
             catch (Exception ex)
             {
                 MessageBox.Show($"Bir Hata Oluştu! {ex.Message}");
             }
+        }
+
+        public void FormuTemizle()
+        {
+            foreach (Control item in this.Controls)
+            {
+                //TODO: Formdaki textbox ve datetimepicklar'ı ilk açıldığı hale getirin
+                item.Text = string.Empty;
+            }
+            //txtAd.Text = string.Empty;
+            //txtSoyad.Text = string.Empty;
+            //txtTckn.Text = string.Empty;
+            //txtTelefon.Text = string.Empty;
+            //txtEmail.Text = string.Empty;
+            //dtpDogumTarihi.Value = DateTime.Now;
+
         }
     }
 }
