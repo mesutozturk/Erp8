960f060e46fb4e2a844482529ab5fcbfc342f868
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2023-02-27 12:26:33 +0300

    .

--- WfaGiris/FormKisiler.Designer.cs
+++ WfaGiris/FormKisiler.Designer.cs
@@ -47,7 +47,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(57, 12);
+            label1.Location = new Point(61, 12);
             label1.Name = "label1";
             label1.Size = new Size(22, 15);
             label1.TabIndex = 0;
@@ -56,7 +56,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(40, 38);
+            label2.Location = new Point(44, 38);
             label2.Name = "label2";
             label2.Size = new Size(39, 15);
             label2.TabIndex = 0;
@@ -65,7 +65,7 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(1, 64);
+            label3.Location = new Point(5, 64);
             label3.Name = "label3";
             label3.Size = new Size(78, 15);
             label3.TabIndex = 0;
@@ -74,7 +74,7 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(43, 90);
+            label4.Location = new Point(47, 90);
             label4.Name = "label4";
             label4.Size = new Size(36, 15);
             label4.TabIndex = 0;
@@ -83,7 +83,7 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(43, 116);
+            label5.Location = new Point(47, 116);
             label5.Name = "label5";
             label5.Size = new Size(36, 15);
             label5.TabIndex = 0;
@@ -92,7 +92,7 @@
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(34, 142);
+            label6.Location = new Point(38, 142);
             label6.Name = "label6";
             label6.Size = new Size(45, 15);
             label6.TabIndex = 0;
@@ -100,45 +100,45 @@
             // 
             // txtAd
             // 
-            txtAd.Location = new Point(85, 6);
+            txtAd.Location = new Point(89, 6);
             txtAd.Name = "txtAd";
             txtAd.Size = new Size(154, 23);
-            txtAd.TabIndex = 1;
+            txtAd.TabIndex = 0;
             // 
             // txtSoyad
             // 
-            txtSoyad.Location = new Point(85, 32);
+            txtSoyad.Location = new Point(89, 32);
             txtSoyad.Name = "txtSoyad";
             txtSoyad.Size = new Size(154, 23);
             txtSoyad.TabIndex = 1;
             // 
             // txtTckn
             // 
-            txtTckn.Location = new Point(85, 84);
+            txtTckn.Location = new Point(89, 84);
             txtTckn.Name = "txtTckn";
             txtTckn.Size = new Size(154, 23);
-            txtTckn.TabIndex = 1;
+            txtTckn.TabIndex = 3;
             // 
             // txtEmail
             // 
-            txtEmail.Location = new Point(85, 110);
+            txtEmail.Location = new Point(89, 110);
             txtEmail.Name = "txtEmail";
             txtEmail.Size = new Size(154, 23);
-            txtEmail.TabIndex = 1;
+            txtEmail.TabIndex = 4;
             // 
             // txtTelefon
             // 
-            txtTelefon.Location = new Point(85, 136);
+            txtTelefon.Location = new Point(89, 136);
             txtTelefon.Name = "txtTelefon";
             txtTelefon.Size = new Size(154, 23);
-            txtTelefon.TabIndex = 1;
+            txtTelefon.TabIndex = 5;
             // 
             // btnKaydet
             // 
-            btnKaydet.Location = new Point(147, 165);
+            btnKaydet.Location = new Point(151, 165);
             btnKaydet.Name = "btnKaydet";
             btnKaydet.Size = new Size(92, 52);
-            btnKaydet.TabIndex = 2;
+            btnKaydet.TabIndex = 6;
             btnKaydet.Text = "Kaydet";
             btnKaydet.UseVisualStyleBackColor = true;
             // 
@@ -146,18 +146,18 @@
             // 
             lstKisiler.FormattingEnabled = true;
             lstKisiler.ItemHeight = 15;
-            lstKisiler.Location = new Point(245, 6);
+            lstKisiler.Location = new Point(249, 6);
             lstKisiler.Name = "lstKisiler";
             lstKisiler.Size = new Size(154, 214);
-            lstKisiler.TabIndex = 3;
+            lstKisiler.TabIndex = 7;
             // 
             // dtpDogumTarihi
             // 
             dtpDogumTarihi.Format = DateTimePickerFormat.Short;
-            dtpDogumTarihi.Location = new Point(85, 58);
+            dtpDogumTarihi.Location = new Point(89, 58);
             dtpDogumTarihi.Name = "dtpDogumTarihi";
             dtpDogumTarihi.Size = new Size(154, 23);
-            dtpDogumTarihi.TabIndex = 4;
+            dtpDogumTarihi.TabIndex = 2;
             // 
             // FormKisiler
             // 
