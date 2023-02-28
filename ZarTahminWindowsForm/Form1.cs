a40237bee94ca60b6ccf156295a596d4a1216ba9
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2023-02-28 14:47:20 +0300

    hızlanan arama

--- ZarTahminWindowsForm/Form1.cs
+++ ZarTahminWindowsForm/Form1.cs
@@ -6,7 +6,7 @@ namespace ZarTahminWindowsForm
         {
             InitializeComponent();
         }
-        private Random _rnd = new Random();
+        private readonly Random _rnd = new Random();
         private int _zar1 = 0, _zar2 = 0, _sayi = 0, _sayac = 0;
         private void btnDondur_Click(object sender, EventArgs e)
         {
@@ -19,6 +19,7 @@ namespace ZarTahminWindowsForm
             tmr1.Start();
             lstSonuc.Items.Clear();
             btnDondur.Enabled = false;
+            tmr1.Interval = 1000;
         }
 
         private void tmr1_Tick(object sender, EventArgs e)
@@ -29,13 +30,17 @@ namespace ZarTahminWindowsForm
             lstSonuc.Items.Insert(0, $"{_sayac:00}) {_zar1}-{_zar2}");
             btn1.Text = _zar1.ToString();
             btn2.Text = _zar2.ToString();
+            this.Text = $"{_sayac}. Tahmin";
             if (_zar1 == _zar2 && _zar1 == _sayi)
             {
-                this.Text = $"{_sayac} denemede {_sayi}-{_sayi} tahmini bulundu";
+                this.Text = $"{_sayac}. denemede {_sayi}-{_sayi} tahmini bulundu";
                 btnDondur.Enabled = true;
                 _sayac = 0;
                 tmr1.Stop();
             }
+
+            var interval = (int)(tmr1.Interval * 0.8);
+            tmr1.Interval = interval <= 1 ? 1 : interval;
         }
     }
 }
