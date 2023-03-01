8ba3b94a5d12536de47bbaa783466887b19bf4a8
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2023-03-01 10:30:54 +0300

    geri sayim beta1

--- Kronometre/Form1.Designer.cs
+++ Kronometre/Form1.Designer.cs
@@ -33,13 +33,20 @@
             btnBaslatDurdur = new Button();
             btnSifirla = new Button();
             tmr1 = new System.Windows.Forms.Timer(components);
+            pbKalanSure = new ProgressBar();
+            cbGeriSayim = new CheckBox();
+            gbKontrol = new GroupBox();
+            cmbSaniye = new ComboBox();
+            cmbDakika = new ComboBox();
+            cmbSaat = new ComboBox();
+            gbKontrol.SuspendLayout();
             SuspendLayout();
             // 
             // lblEkran
             // 
             lblEkran.AutoSize = true;
             lblEkran.Font = new Font("Consolas", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
-            lblEkran.Location = new Point(26, 44);
+            lblEkran.Location = new Point(12, 100);
             lblEkran.Name = "lblEkran";
             lblEkran.Size = new Size(159, 34);
             lblEkran.TabIndex = 0;
@@ -48,9 +55,9 @@
             // btnBaslatDurdur
             // 
             btnBaslatDurdur.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
-            btnBaslatDurdur.Location = new Point(206, 24);
+            btnBaslatDurdur.Location = new Point(177, 80);
             btnBaslatDurdur.Name = "btnBaslatDurdur";
-            btnBaslatDurdur.Size = new Size(83, 79);
+            btnBaslatDurdur.Size = new Size(113, 79);
             btnBaslatDurdur.TabIndex = 1;
             btnBaslatDurdur.Text = "Başlat";
             btnBaslatDurdur.UseVisualStyleBackColor = true;
@@ -59,27 +66,92 @@
             // btnSifirla
             // 
             btnSifirla.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
-            btnSifirla.Location = new Point(295, 24);
+            btnSifirla.Location = new Point(294, 80);
             btnSifirla.Name = "btnSifirla";
-            btnSifirla.Size = new Size(83, 79);
+            btnSifirla.Size = new Size(113, 79);
             btnSifirla.TabIndex = 1;
             btnSifirla.Text = "Sıfırla";
             btnSifirla.UseVisualStyleBackColor = true;
+            btnSifirla.Click += btnSifirla_Click;
             // 
             // tmr1
             // 
             tmr1.Tick += tmr1_Tick;
             // 
+            // pbKalanSure
+            // 
+            pbKalanSure.BackColor = SystemColors.ActiveCaption;
+            pbKalanSure.Location = new Point(12, 165);
+            pbKalanSure.MarqueeAnimationSpeed = 50;
+            pbKalanSure.Name = "pbKalanSure";
+            pbKalanSure.Size = new Size(395, 23);
+            pbKalanSure.Style = ProgressBarStyle.Continuous;
+            pbKalanSure.TabIndex = 2;
+            // 
+            // cbGeriSayim
+            // 
+            cbGeriSayim.AutoSize = true;
+            cbGeriSayim.Location = new Point(12, 12);
+            cbGeriSayim.Name = "cbGeriSayim";
+            cbGeriSayim.RightToLeft = RightToLeft.Yes;
+            cbGeriSayim.Size = new Size(117, 19);
+            cbGeriSayim.TabIndex = 3;
+            cbGeriSayim.Text = "Geri Sayım Modu";
+            cbGeriSayim.UseVisualStyleBackColor = true;
+            cbGeriSayim.CheckedChanged += cbGeriSayim_CheckedChanged;
+            // 
+            // gbKontrol
+            // 
+            gbKontrol.Controls.Add(cmbSaniye);
+            gbKontrol.Controls.Add(cmbDakika);
+            gbKontrol.Controls.Add(cmbSaat);
+            gbKontrol.Location = new Point(177, 4);
+            gbKontrol.Name = "gbKontrol";
+            gbKontrol.Size = new Size(230, 70);
+            gbKontrol.TabIndex = 4;
+            gbKontrol.TabStop = false;
+            gbKontrol.Text = "Geri Sayım Kontrolleri";
+            gbKontrol.Visible = false;
+            // 
+            // cmbSaniye
+            // 
+            cmbSaniye.FormattingEnabled = true;
+            cmbSaniye.Location = new Point(164, 22);
+            cmbSaniye.Name = "cmbSaniye";
+            cmbSaniye.Size = new Size(60, 23);
+            cmbSaniye.TabIndex = 0;
+            // 
+            // cmbDakika
+            // 
+            cmbDakika.FormattingEnabled = true;
+            cmbDakika.Location = new Point(85, 22);
+            cmbDakika.Name = "cmbDakika";
+            cmbDakika.Size = new Size(60, 23);
+            cmbDakika.TabIndex = 0;
+            // 
+            // cmbSaat
+            // 
+            cmbSaat.FormattingEnabled = true;
+            cmbSaat.Location = new Point(6, 22);
+            cmbSaat.Name = "cmbSaat";
+            cmbSaat.Size = new Size(60, 23);
+            cmbSaat.TabIndex = 0;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(402, 390);
+            ClientSize = new Size(419, 205);
+            Controls.Add(gbKontrol);
+            Controls.Add(cbGeriSayim);
+            Controls.Add(pbKalanSure);
             Controls.Add(btnSifirla);
             Controls.Add(btnBaslatDurdur);
             Controls.Add(lblEkran);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
+            gbKontrol.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -90,5 +162,11 @@
         private Button btnBaslatDurdur;
         private Button btnSifirla;
         private System.Windows.Forms.Timer tmr1;
+        private ProgressBar pbKalanSure;
+        private CheckBox cbGeriSayim;
+        private GroupBox gbKontrol;
+        private ComboBox cmbSaniye;
+        private ComboBox cmbDakika;
+        private ComboBox cmbSaat;
     }
 }
--- Kronometre/Form1.cs
+++ Kronometre/Form1.cs
@@ -7,23 +7,35 @@ namespace Kronometre
             InitializeComponent();
         }
 
-        bool _calisiyorMu = false, _duraklatildiMi = false;
-        DateTime _oncekiZaman, _sonrakiZaman, _kronometre = new DateTime();
+        private bool _calisiyorMu = false;
+        private DateTime _oncekiZaman, _sonrakiZaman, _kronometre = new DateTime();
 
         private void btnBaslatDurdur_Click(object sender, EventArgs e)
         {
             _calisiyorMu = !_calisiyorMu;
             if (_calisiyorMu)
             {
+                if (cbGeriSayim.Checked)
+                    _kronometre = new DateTime(1, 1, 1, cmbSaat.SelectedIndex, cmbDakika.SelectedIndex,
+                        cmbSaniye.SelectedIndex);
+                else
+                {
+                    _kronometre = new DateTime();
+                }
                 _oncekiZaman = DateTime.Now;
                 tmr1.Start();
                 btnBaslatDurdur.Text = "Duraklat";
+                btnBaslatDurdur.FlatStyle = FlatStyle.Flat;
+                btnBaslatDurdur.BackColor = Color.Chartreuse;
+                btnSifirla.Enabled = false;
             }
             else
             {
-                _duraklatildiMi = true;
                 tmr1.Stop();
                 btnBaslatDurdur.Text = "Baþlat";
+                btnBaslatDurdur.FlatStyle = FlatStyle.Standard;
+                btnBaslatDurdur.BackColor = Color.IndianRed;
+                btnSifirla.Enabled = true;
             }
         }
 
@@ -31,10 +43,41 @@ namespace Kronometre
         {
             _sonrakiZaman = _oncekiZaman;
             _oncekiZaman = DateTime.Now;
-
             TimeSpan fark = _oncekiZaman - _sonrakiZaman;
-            _kronometre = _kronometre.AddMilliseconds(fark.TotalMilliseconds);
+
+            if (cbGeriSayim.Checked)
+            {
+                _kronometre = _kronometre.AddMilliseconds(fark.TotalMilliseconds * -1);
+            }
+            else
+            {
+                _kronometre = _kronometre.AddMilliseconds(fark.TotalMilliseconds);
+            }
             lblEkran.Text = _kronometre.ToString("mm:ss:fff");
+        }
+
+        private void btnSifirla_Click(object sender, EventArgs e)
+        {
+            _kronometre = new DateTime();
+            lblEkran.Text = _kronometre.ToString("mm:ss:fff");
+        }
+
+        private void cbGeriSayim_CheckedChanged(object sender, EventArgs e)
+        {
+            gbKontrol.Visible = cbGeriSayim.Checked;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= 60; i++)
+            {
+                cmbSaniye.Items.Add(i);
+                cmbDakika.Items.Add(i);
+                cmbSaat.Items.Add(i);
+            }
+            cmbSaniye.SelectedIndex = 0;
+            cmbDakika.SelectedIndex = 0;
+            cmbSaat.SelectedIndex = 0;
         }
     }
 }
