03d05e9a288b5b3a5864e49c65f45180d96f5675
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2023-03-02 10:36:34 +0300

    .

---
+++ Kalitim.Lib/Dikdortgen.cs
@@ -0,0 +1,11 @@
+﻿namespace Kalitim.Lib;
+
+public class Dikdortgen : Sekil
+{
+    public double Y { get; set; } 
+    public override double AlanHesapla() => X * Y;
+    public override double CevreHesapla()
+    {
+        return 2 * (X + Y);
+    }
+}
---
+++ Kalitim.Lib/Kare.cs
@@ -0,0 +1,9 @@
+﻿namespace Kalitim.Lib;
+
+public class Kare : Sekil
+{
+    public override double CevreHesapla()//
+    {
+        return 4 * X;
+    }
+}
