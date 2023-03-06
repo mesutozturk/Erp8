292dd4d531c8625ce70c8755d4aca969021a5569
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2023-03-06 09:47:01 +0300

    static ve singleton

---
+++ StatikNesneler/SingletonDeneme.cs
@@ -0,0 +1,28 @@
+﻿namespace StatikNesneler;
+public class SingletonDeneme
+{
+    public Guid Id { get; set; }
+    
+    private SingletonDeneme()
+    {
+        Thread.Sleep(10 * 1000);
+        this.Id = Guid.NewGuid();
+    }
+    private static SingletonDeneme _singletonDeneme;
+    public static SingletonDeneme GetInstance()
+    {
+        if (_singletonDeneme == null)
+            _singletonDeneme = new SingletonDeneme();
+        return _singletonDeneme;
+    }
+
+    public static SingletonDeneme Instance
+    {
+        get
+        {
+            if (_singletonDeneme == null)
+                _singletonDeneme = new SingletonDeneme();
+            return _singletonDeneme;
+        }
+    }
+}
